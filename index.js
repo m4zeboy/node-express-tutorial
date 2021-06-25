d88c99685cc41465863cce574eea2602e8bc9655
unknown <moises775azevedo@gmail.com>
2021-06-25 19:01:19 -0300

    Make a 'POST' Request

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 const express = require('express');
+const shortid = require('shortid')
+
 const server = express();
+server.use(express.json())
 
 const PORT = 5000;
 
-server.get('/', (req,res)=>{
-    return res.json({ hello: 'world' })
+const channels = [];
+const lessons = [];
+
+server.get('/', (req, res) => {
+   return res.json({ hello: 'world' })
 })
 
 server.get('/hello', (req, res) => {
-    res.json({ hello: "future senior software developer"})
-}) 
+   res.json({ hello: "future senior software developer" })
+})
+
+server.post('/api/channels', (req, res) => {
+   const channelInfo = req.body;
+   channelInfo.id = shortid.generate()
+   channels.push(channelInfo)
+   return res.status(201).json(channelInfo)
+})
 
 server.listen(PORT, () => console.log(`\nServer is running on https://localhost:${PORT}`))
