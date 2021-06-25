40c57a70e0c5f104647834cd66803a56cd4bc585
unknown <moises775azevedo@gmail.com>
2021-06-25 11:39:10 -0300

    Setting up a 'GET' endpoint'

--- index.js
+++ index.js
@@ -7,4 +7,8 @@ server.get('/', (req,res)=>{
     return res.json({ hello: 'world' })
 })
 
+server.get('/hello', (req, res) => {
+    res.json({ hello: "future senior software developer"})
+}) 
+
 server.listen(PORT, () => console.log(`\nServer is running on https://localhost:${PORT}`))
